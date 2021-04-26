0c07557fdeaf5fc3381b816c2fd9605c35ebd8e6
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-26 19:44:42 +0900

    Add: styled-components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes';
+import GlobalStyle from './Styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import Router from './Routes';
+import theme from './Styles/theme';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+  </>,
+  document.getElementById('root')
+);
