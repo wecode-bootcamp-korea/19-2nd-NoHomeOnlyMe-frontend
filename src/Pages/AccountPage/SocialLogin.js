da52e862814278f3d8f0589282938009de61b2d6
ljh95 <ilegwmam@gmail.com>
2021-04-26 15:59:33 +0900

    Add: first commit. 초기세팅 완료

---
+++ src/Pages/AccountPage/SocialLogin.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SocialLogin extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          {/* <Route exact path="/" component={Main} /> */}
+        </Switch>
+      </Router>
+    );
+  }
+}
