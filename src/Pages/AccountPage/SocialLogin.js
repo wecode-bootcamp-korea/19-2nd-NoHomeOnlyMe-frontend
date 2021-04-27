b660836d2edd14f4086ff248c343143dc6e0c8b6
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-27 19:25:20 +0900

    ADD Social Login KaKao

--- src/Pages/AccountPage/SocialLogin.js
+++ src/Pages/AccountPage/SocialLogin.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-export default class SocialLogin extends Component {
-  render() {
-    return (
-      <div>
+function SocialLogin() {
+  const { Kakao } = window;
+  const kakaoLogin = () => {
+    Kakao.Auth.login({
+      success: res => {
+        console.log(res);
+      },
+      fail: err => {
+        console.log(err);
+      },
+    });
+  };
 
-      </div>
-    )
-  }
+  useEffect(() => {
+    console.log(window.Kakao);
+  }, []);
+
+  return <Button onClick={kakaoLogin}>카카오톡으로 시작</Button>;
 }
+
+export default SocialLogin;
+
+const Button = styled.button`
+  width: 170px;
+  height: 50px;
+  color: rgb(34, 34, 34);
+  background-color: rgb(247, 229, 6);
+  font-size: 14px;
+  border: 0px;
+`;
