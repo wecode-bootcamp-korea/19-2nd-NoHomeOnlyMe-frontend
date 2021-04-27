b9a44ef11c44196b15787eb98ab93297b7f19f45
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-27 19:25:20 +0900

    ADD Social Login KaKao

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Pages/AccountPage/Login';
+import SocialLogin from './Pages/AccountPage/SocialLogin';
 
 export default class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Login} />
-          {/* <Route exact path="/login" component={Login} /> */}
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/socialLogin" component={SocialLogin} />
         </Switch>
       </Router>
     );
